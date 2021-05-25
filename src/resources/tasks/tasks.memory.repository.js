db02a159aa3788a641679f349b3216fb1d6acb6f
Dmitry Yermoshkin <spanb4@gmail.com>
2021-05-25 20:58:03 +0300

    docs: add jsdoc for tasks

--- src/resources/tasks/tasks.memory.repository.js
+++ src/resources/tasks/tasks.memory.repository.js
@@ -2,8 +2,22 @@ const Task = require('./tasks.model');
 
 Task.instances = [];
 
+/**
+ * Tasks memory repository module
+ * @module tasks_memory_repository
+ */
+
+/**
+ * Get all tasks
+ * @return {Promise<Task[]>} All tasks array
+ */
 const getAll = async () => Task.instances;
 
+/**
+ * Get task by ID
+ * @param {string} id Task ID
+ * @return {(Promise<Task>|Error)} Received task or error
+ */
 const getById = async (id) => {
     const task = Task.instances.find((_task) => _task.id === id);
 
@@ -14,12 +28,35 @@ const getById = async (id) => {
     return task;
 };
 
+/**
+ * Create new task
+ * @param {Object} taskData Data for task creation
+ * @param {string} taskData.title Task title
+ * @param {string} taskData.order Task order
+ * @param {string} taskData.description Task description
+ * @param {string} taskData.userId Task userId
+ * @param {string} taskData.boardId Task boardId
+ * @param {string} taskData.columnId Task columnId
+ * @return {Promise<Task>} Created task
+ */
 const create = async (taskData) => {
     const task = await new Task(taskData);
 
     return task;
 };
 
+/**
+ * Update task's data
+ * @param {string} id Task ID
+ * @param {Object} taskData Data for task creation
+ * @param {string} taskData.title Task title
+ * @param {string} taskData.order Task order
+ * @param {string} taskData.description Task description
+ * @param {string} taskData.userId Task userId
+ * @param {string} taskData.boardId Task boardId
+ * @param {string} taskData.columnId Task columnId
+ * @return {(Promise<Task>|Error)} Updated task or error
+ */
 const update = async (id, newTaskData) => {
     const task = await getById(id);
 
@@ -37,6 +74,11 @@ const update = async (id, newTaskData) => {
     return task;
 };
 
+/**
+ * Remove task
+ * @param {string} id Task ID
+ * @return {(Promise<Task>|Error)} Removed task or error
+ */
 const remove = async (id) => {
     const removedTask = await getById(id);
 
--- src/resources/tasks/tasks.model.js
+++ src/resources/tasks/tasks.model.js
@@ -1,5 +1,17 @@
 const { v4: uuidv4 } = require('uuid');
 
+/**
+ * Task type definition
+ * @typedef {Object} Task
+ * @property {string} id Task ID
+ * @property {string} title Task title
+ * @property {string} order Task order
+ * @property {string} description Task description
+ * @property {string} userId Task userId
+ * @property {string} boardId Task boardId
+ * @property {string} columnId Task columnId
+ */
+
 class Task {
     constructor({ title, order, description, userId, boardId, columnId }) {
         this.id = uuidv4();
