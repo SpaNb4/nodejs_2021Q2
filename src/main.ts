431daf2e3ec096dbfd267779c053d043518f5679
Dmitry Yermoshkin <spanb4@gmail.com>
2021-07-05 18:19:07 +0300

    feat: add create admin function

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/all-exception.filter';
+import { createAdmin } from './common/create-admin';
 
 dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalFilters(new AllExceptionsFilter());
+    createAdmin();
     await app.listen(process.env['PORT']!);
 }
 bootstrap();
