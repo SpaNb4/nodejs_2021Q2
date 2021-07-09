0446d4a0dd6e40c2c36e3e5fca75a49975ebfac0
Dmitry Yermoshkin <spanb4@gmail.com>
2021-07-09 09:47:25 +0300

    feat: prepare for fastify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/all-exception.filter';
@@ -8,12 +10,17 @@ import { morganLog, myLogger } from './logger/logger';
 dotenv.config();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
-        logger: myLogger,
-    });
+    const app = await NestFactory.create(
+        AppModule,
+        process.env['USE_FASTIFY'] === 'true' ? new FastifyAdapter() : new ExpressAdapter(),
+        {
+            logger: myLogger,
+        }
+    );
     app.use(morganLog);
     app.useGlobalFilters(new AllExceptionsFilter());
     createAdmin();
-    await app.listen(process.env['PORT']!);
+    await app.listen(process.env['PORT']!, '0.0.0.0');
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
